f28ebeddbf6918354854616313cfa40c8ad85b0f
hirokazunakata <hekomikunn@gmail.com>
2017-02-12 13:11:39 +0000

    Task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only:[:edit, :update]
+  before_action :check_user, only:[:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -20,9 +21,31 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "edited your profile!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-end
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :age, :profile, :location)
+    end
   
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    def check_user
+      if current_user != @user
+        redirect_to root_url
+      end
+    end
+end
+
